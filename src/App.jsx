321a6e916d64ff0ae331bb4d4671cd6340a698a8
Nishanaaz <nishan22@navgurukul.org>
2023-07-28 10:40:25 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import LoginForm from './component/LoginForm/LoginForm'
+
+function App() {
+
+  return (
+    <>
+  
+     <LoginForm/>   
+    </>
+  )
+}
+
+export default App
---
+++ src/component/LoginForm/LoginForm.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from "./LoginForm.module.css"
+
+const LoginForm = () => {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default LoginForm
